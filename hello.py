cb22df274f48c6e54ee658c3be436f43dd1773b0
Wei Wang <wang0109@gmail.com>
2016-11-09 23:33:52 -0800

    add one py

---
+++ hello.py
@@ -0,0 +1,11 @@
+import sublime
+import sublime_plugin
+
+
+# Use console: view.run_command('example') run this within
+# Sublime.. example -> ExampleCommond is auto-deduced somehow..
+# It does: Go to file begin and insert hello world...
+class ExampleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+	self.view.insert(edit, 0, "Hello, World!")
+
